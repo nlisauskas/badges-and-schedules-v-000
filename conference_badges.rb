a7763cc81c6f5080f77a5780c664a220c6751f39
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 10:58:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
+def batch_badge_creator(attendees)
   name_array = []
-  names.each do |name|
+  attendees.each do |name|
     name_array << name
   end
   name_array
 end
-
